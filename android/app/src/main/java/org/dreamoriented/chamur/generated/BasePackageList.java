db7a841d4edb46d662222139e436e1490b05e8fd
Burak Tokak <iletisim@buraktokak.com>
2021-03-28 00:22:14 +0300

    fix android

--- android/app/src/main/java/org/dreamoriented/chamur/generated/BasePackageList.java
+++ android/app/src/main/java/org/dreamoriented/chamur/generated/BasePackageList.java
@@ -25,8 +25,10 @@ public class BasePackageList {
         new expo.modules.notifications.NotificationsPackage(),
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.screenorientation.ScreenOrientationPackage(),
+        new expo.modules.securestore.SecureStorePackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
-        new expo.modules.sqlite.SQLitePackage()
+        new expo.modules.sqlite.SQLitePackage(),
+        new expo.modules.updates.UpdatesPackage()
     );
   }
 }
